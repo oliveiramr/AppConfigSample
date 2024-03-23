0d05c8016f4ada3aa5eb595a3aff7c686eda0ddc
Murilo R. Oliveira <oliveira.mr@outlook.com.br>
2024-03-23 12:53:52 -0300

    Initial Commit

---
+++ AppConfigSample/AppConfigSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppConfigSampleApp.swift
+//  AppConfigSample
+//
+//  Created by Murilo Ribeiro de Oliveira on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AppConfigSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
