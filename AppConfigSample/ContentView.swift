11b6bbe1d1e7000b15405a2f3ba47d84a782daa6
Murilo R. Oliveira <oliveira.mr@outlook.com.br>
2024-03-23 13:23:13 -0300

    Update ContentView.swift

--- AppConfigSample/ContentView.swift
+++ AppConfigSample/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        VStack(spacing: 10) {
+            Text("Objeto Gerenciado").font(.largeTitle)
+            Text(getObject() ?? "Sem objeto gerenciado").font(.title)
+
         }
-        .padding()
+    }
+
+     func fetchAppConfig() -> [String: AnyObject] {
+
+           return UserDefaults.standard.dictionary(forKey: "com.apple.configuration.managed") as? [String: AnyObject] ?? [:]
+       }
+
+     func getObject() -> (String)? {
+
+        let appConfig = fetchAppConfig()
+
+        guard let managedEndpoint = appConfig["endpoint"] as? String else {
+            return nil
+        }
+
+        return managedEndpoint
     }
 }
 
